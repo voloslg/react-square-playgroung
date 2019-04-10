76f8f8f135a80d14ec0b35956f1aec66449ce918
volosgoto <volosgoto@yahoo.com>
2019-04-10 10:01:35 +0300

    add refs to square

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Square from "./Square";
-import Refs from "./playgroung/Refs.js";
+// import Refs from "./playgroung/Refs.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Square />
+        <Square />
         <Square initialColor={"red"} />
-        <Square initialColor={"green"} /> */}
-        <Refs />
+        <Square initialColor={"green"} />
+
+        {/* <Refs /> */}
       </div>
     );
   }
--- src/Square.js
+++ src/Square.js
@@ -2,28 +2,44 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Square.css";
 
-const getTitile = () => "Function title";
-
 export default class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentColor: this.props.initialColor
+    };
+  }
+
+  setCurrentColor = () => {
+    console.log("setCurrentColor", this.refs.colorInput.value);
+    let color = this.refs.colorInput.value;
+    this.setState({
+      currentColor: color
+    });
+  };
+
   render() {
-    const theTitle = <strong>Bold title</strong>;
-
-    let elements = [<p>1 element</p>, <p>2 element</p>, <p>3 element</p>];
-
     let style = {
-      width: "200px",
-      height: "200px",
+      width: "100px",
+      height: "100px",
       border: "2px solid black",
-      backgroundColor: this.props.initialColor
+      backgroundColor: this.state.currentColor
     };
 
     return (
-      <div className="Square" style={style}>
-        {/* {getTitile()} */}
-        {/* {2 + 2} */}
-        {/* {theTitle} */}
-        {/* {elements} */}
-        {this.props.showTitle && <strong>TITLE</strong>}
+      <div>
+        <div className="Square" style={style} />
+        <div className="SquareControls">
+          <input
+            // ref={input => (this.colorInput = input)}
+            ref="colorInput"
+            type="text"
+            name=""
+            id=""
+            placeholder="Color..."
+          />
+          <button onClick={this.setCurrentColor}>Ok</button>
+        </div>
       </div>
     );
   }
